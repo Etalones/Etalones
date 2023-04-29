91fb5c1e779fbc9c4d6d6f4bdef7d2559ff61d5a
Ar4ySmoke <developerar4y@gmail.com>
2023-04-29 16:23:11 +0300

    add file
    add file

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$("#btn-popup").click(()=> {
+        $(".popup").toggleClass("hidden")
+}) 
+$("#btn-popup_anketa").click(()=> {
+        $(".popup").toggleClass("hidden")
+}) 
+
+$("#btn-close").click(()=> {
+    $(".popup").toggleClass("hidden")
+})
+$("#cancel").click(()=> {
+    $(".popup").toggleClass("hidden")
+})
+
+$("#btn-popup_01").click(()=> {
+        $(".popup").toggleClass("hidden")
+}) 
+$("#btn-close_01").click(()=> {
+    $(".popup").toggleClass("hidden")
+})
+
+
