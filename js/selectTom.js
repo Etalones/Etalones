246fa9893df0966f092ede35af144449aea1c9d2
Artur Fedcenco <developerar4y@gmail.com>
2023-11-21 23:47:31 +0200

    predeploy 21:11 23:47

--- js/selectTom.js
+++ js/selectTom.js
@@ -1,13 +1,51 @@
 document.querySelectorAll(".dropdown-submenu").forEach(function (item) {
   item.addEventListener("mouseover", function () {
     const dropdownMenu = item.querySelector(".dropdown-menu");
-    dropdownMenu.classList.add("show");
-    dropdownMenu.style.opacity = "1";
+    showDropdown(dropdownMenu);
   });
 
   item.addEventListener("mouseout", function () {
     const dropdownMenu = item.querySelector(".dropdown-menu");
-    dropdownMenu.classList.remove("show");
-    dropdownMenu.style.opacity = "0";
+    hideDropdown(dropdownMenu);
+  });
+
+  // Добавим обработчик клика для мобильных устройств
+  item.addEventListener("click", function (event) {
+    const dropdownMenu = item.querySelector(".dropdown-menu");
+
+    if (!isMobile()) {
+      // Если не мобильное устройство, покажем/скроем меню при наведении
+      showDropdown(dropdownMenu);
+    } else {
+      // Если мобильное устройство, предотвратим переход по ссылке
+      event.preventDefault();
+
+      // Переключим видимость меню при клике
+      toggleDropdown(dropdownMenu);
+    }
   });
 });
+
+// Добавим функции для управления видимостью меню
+function showDropdown(dropdownMenu) {
+  dropdownMenu.classList.add("show");
+  dropdownMenu.style.opacity = "1";
+}
+
+function hideDropdown(dropdownMenu) {
+  dropdownMenu.classList.remove("show");
+  dropdownMenu.style.opacity = "0";
+}
+
+function toggleDropdown(dropdownMenu) {
+  if (dropdownMenu.classList.contains("show")) {
+    hideDropdown(dropdownMenu);
+  } else {
+    showDropdown(dropdownMenu);
+  }
+}
+
+// Добавим функцию для проверки, является ли устройство мобильным
+function isMobile() {
+  return window.innerWidth <= 767; // Измените значение, если необходимо
+}
