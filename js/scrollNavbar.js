2bdc2bb036345f655b605f65024d8795d9d9c5c2
Artur Fedcenco <developerar4y@gmail.com>
2023-11-21 21:47:49 +0200

    predeploy 21.11 21:47

--- js/scrollNavbar.js
+++ js/scrollNavbar.js
@@ -20,4 +20,20 @@ window.addEventListener("scroll", function () {
     }
   });
 });
+ // Функция для прокрутки страницы вверх
+    function scrollToTop() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Добавляет плавность прокрутки (поддерживается в современных браузерах)
+      });
+    }
 
+    // Отслеживание прокрутки и отображение/скрытие кнопки вверх
+    window.onscroll = function() {
+      var scrollToTopBtn = document.getElementById('scrollToTopBtn');
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = 'block';
+      } else {
+        scrollToTopBtn.style.display = 'none';
+      }
+    };
