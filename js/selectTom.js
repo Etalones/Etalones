87ae053a41948717dade82c9c391978c74d76048
Artur Fedcenco <developerar4y@gmail.com>
2023-11-23 15:52:04 +0200

    add3pagesNewNav

--- js/selectTom.js
+++ js/selectTom.js
@@ -1,13 +1,62 @@
-document.querySelectorAll(".dropdown-submenu").forEach(function (item) {
-  item.addEventListener("mouseover", function () {
-    const dropdownMenu = item.querySelector(".dropdown-menu");
-    dropdownMenu.classList.add("show");
-    dropdownMenu.style.opacity = "1";
+// document.querySelectorAll(".dropdown-submenu").forEach(function (item) {
+//   item.addEventListener("mouseover", function () {
+//     const dropdownMenu = item.querySelector(".dropdown-menu");
+//     dropdownMenu.classList.add("show");
+//     dropdownMenu.style.opacity = "1";
+//   });
+
+//   item.addEventListener("mouseout", function () {
+//     const dropdownMenu = item.querySelector(".dropdown-menu");
+//     dropdownMenu.classList.remove("show");
+//     dropdownMenu.style.opacity = "0";
+//   });
+// });
+document.addEventListener("DOMContentLoaded", function () {
+  const burgerMenuButton = document.querySelector(".burgerMenu");
+  const close = document.querySelector("#close__burger");
+  const navigationMenu = document.querySelector("#burgermenu");
+
+  close.addEventListener("click", function (event) {
+    event.stopPropagation();
+    closeMenu();
   });
 
-  item.addEventListener("mouseout", function () {
-    const dropdownMenu = item.querySelector(".dropdown-menu");
-    dropdownMenu.classList.remove("show");
-    dropdownMenu.style.opacity = "0";
+  // Добавляем обработчик для элементов меню, чтобы предотвратить всплытие события
+  navigationMenu.addEventListener("click", function (event) {
+    event.stopPropagation();
   });
+
+  function closeMenu() {
+    navigationMenu.style.opacity = 0;
+    navigationMenu.style.visibility = "hidden";
+    document.removeEventListener("click", closeMenu);
+
+    // Восстанавливаем прокрутку при закрытии меню
+    document.body.style.overflow = "auto";
+  }
+
+  burgerMenuButton.addEventListener("click", function () {
+    navigationMenu.style.opacity = 1;
+    navigationMenu.style.visibility = "visible";
+
+    // Отключаем плавность прокрутки при открытии меню
+    document.body.style.overflow = "hidden";
+    document.body.style.scrollBehavior = "auto";
+
+    setTimeout(function () {
+      document.addEventListener("click", closeMenu);
+
+      // Восстанавливаем плавность прокрутки через небольшую задержку
+      setTimeout(function () {
+        document.body.style.scrollBehavior = "smooth";
+      }, 100);
+    }, 100);
+  });
+
+  // Показываем меню после загрузки страницы
+  navigationMenu.style.opacity = 1;
+  navigationMenu.style.visibility = "visible";
+
+  // Закрываем меню при загрузке страницы
+  closeMenu();
 });
